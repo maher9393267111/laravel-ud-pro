4daf8e8039a044517df902d57b8d921147230f38
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-01-11 13:37:02 +0300

    create blog to databse start work anbd showall in table start

--- routes/web.php
+++ routes/web.php
@@ -136,6 +136,7 @@ Route::controller(AboutController::class)->group(function () {
  Route::controller(BlogController::class)->group(function () {
     Route::get('/all/blog', 'AllBlog')->name('all.blog');
     Route::get('/add/blog', 'AddBlog')->name('add.blog');
+    Route::post('/store/blog', 'StoreBlog')->name('store.blog');
 
 
 });
